88ed8e88cffc3f2e26681433f2e924c2b6b37b90
Guilherme Gehring <guilherme.gehring@gmail.com>
2022-04-06 09:35:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { CapacitorUpdater } from 'capacitor-updater'
+CapacitorUpdater.notifyAppReady()
+export default {
+  name: 'App'
+}
+</script>
